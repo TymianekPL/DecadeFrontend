5af61baaaa40730bd6b7b8792ba7105cf1389b5a
Tymianek <70819917+TymianekPL@users.noreply.github.com>
2024-09-10 09:37:16 +0200

    Update Contact.tsx
    add details

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,6 +5,11 @@ interface ContactProps {
 const Contact = ({}: ContactProps) => (
     <div>
         <h1>Contact</h1>
+        <p>
+             <div>Email: <a href="mailto:tymi@tymi.org">tymi@tymi.org</a></div>
+             <div>Discord: <a href="https://discord.gg/9sAgruPPWq">Decade</a></div>
+             <div>Github: <a href="Https://github.com/TymianekPL/DecadeFrontend">TymianekPL/DecadeFrontend</a></div>
+        </p>
     </div>
 );
 
